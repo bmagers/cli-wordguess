bd006eb257b2e12f4355f98c05bdf51eb3b95912
Bryan Magers <bmagers@users.noreply.github.com>
2018-10-25 15:13:52 -0700

    Minor fixes.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var inquirer = require("inquirer");
 var Word = require("./Word.js");
-var word = new Word("test");
 var guessesRemaining = 10;
+var wordsToUse = ["CHICAGO", "DALLAS", "HOUSTON", "MIAMI", "PHILADELPHIA", "ATLANTA", "BOSTON", "PHOENIX", "DETROIT", "SEATTLE", "MINNEAPOLIS", "DENVER", "BALTIMORE", "CHARLOTTE", "PORTLAND", "OAKLAND", "TAMPA", "ORLANDO", "PITTSBURGH", "SACRAMENTO", "CINCINNATI", "CLEVELAND", "RALEIGH", "MILWAUKEE", "NASHVILLE", "AUSTIN", "INDIANAPOLIS", "ALBUQUERQUE"];
+var word;
+
+function newWord() {
+  var currentWord = wordsToUse[Math.floor(Math.random() * (wordsToUse.length - 1))];
+  word = new Word(currentWord);
+  console.log(word.display() + "\n");
+  guess();
+}
 
 function guess() {
   inquirer.prompt([
@@ -10,20 +18,24 @@ function guess() {
       message: "Please guess a letter"
     }
   ]).then(function(answer) {
-    if (word.guess(answer.letter)) {
-      console.log("Correct!");
+    if (word.guess(answer.letter.toUpperCase())) {
+      console.log("Correct!\n");
     } else {
       guessesRemaining--;
-      console.log("Incorrect. Guesses remaining: " + guessesRemaining);
+      console.log("Incorrect. Guesses remaining: " + guessesRemaining + "\n");
     }
     console.log(word.display() + "\n");
     if (guessesRemaining === 0) {
       console.log("You are out of guesses.");
     } else {
-      word.remaining() > 0 ? guess() : console.log("You guessed the word!");
+      if (word.remaining() > 0) {
+        guess();
+      } else {
+        console.log("You guessed the word!\nHere's a new word.\n");
+        newWord();
+      }
     } 
   });
 }
 
-console.log(word.display());
-guess();
+newWord();
